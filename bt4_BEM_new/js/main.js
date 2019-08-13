583a0346a25d14e78e66bcae7dc8b2dad3834871
SergeyZhur <ser90593696@yandex.ru>
2019-08-13 16:18:56 +0300

    end

--- bt4_BEM_new/js/main.js
+++ bt4_BEM_new/js/main.js
@@ -26,8 +26,58 @@ $(document).ready(function() {
     slidesToScroll: 3,
     arrows:false,
     dots:true,
-    dotsClass:'dots-style'
+    dotsClass:'dots-style',
+    responsive: [{
+        breakpoint:1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }]
   });
 
+// меню
+$('ul.menu a[href^="#"').click(function() {
+  var target = $(this).attr('href');
+  $('html, body').animate({
+    scrollTop: $(target).offset().top
+  }, 500);
+  $('ul.menu a[href^="#"').css({'color':'#212121'});
+  $(this).css({'color':'#004bee'});
+  return false;
 })
+// выпадающие меню
+$('.menu-icon').click(function(){
+  $('nav').slideToggle(500);
+  $('ul.menu').css({
+    'display':'flex', 'flex-direction':'column'
+  })
+  if($('.menu-icon').html()=='<i class="fas fa-bars"></i>') {
+    $(this).html('<i class="fas fa-user-times"></i>')
+  } else {
+    $(this).html('<i class="fas fa-bars"></i>')
+  }
+})
+// кнопка
+$(window).scroll(function() {
+  if ($(this).scrollTop() !=0)
+  $('#toTop').fadeIn();
+  else
+  $('#toTop').fadeOut();
+});
+$('#toTop').click(function(){
+  $('body,html').animate({
+    scrollTop:0
+  }, 800);
+});
+
+})
+
 
