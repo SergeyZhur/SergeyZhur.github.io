544543c8be489cbeb4b1938685d066b6d3fd2437
SergeyZhur <ser90593696@yandex.ru>
2019-11-10 14:53:54 +0300

    second Block Ready

--- shopNike/index.js
+++ shopNike/index.js
@@ -15,3 +15,25 @@ button.addEventListener('click', clickOnButton)
 // }
 
 // button.addEventListener('click', as)
+
+var btn = document.getElementById('btn-header-one');
+var btn2 = document.getElementById('btn-header-two');
+var header = document.getElementById('header');
+
+btn.onclick = function() {
+  header.classList.add('header-background-girl');
+  pupap.classList.add('pupap-information-girl');
+  
+}
+btn2.onclick = function() {
+  header.classList.remove('header-background-girl');
+  pupap.classList.remove('pupap-information-girl');
+}
+
+
+// document.onclick = function() {
+//   // if(event.target == header) {
+//   // pupap.style.background='red';
+//   // }
+//   pupap.style.background = "red";
+// }
