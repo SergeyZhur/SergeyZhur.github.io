179061b263e80a1b59dcefb1f38c517f67ea47dc
Sergei <ser90593696@yandex.ru>
2020-05-04 20:04:12 +0300

    Исправления по 3 уроку д з

--- MageGame/Код и магия/js/setup.js
+++ MageGame/Код и магия/js/setup.js
@@ -1,12 +1,9 @@
 'use strict';
-// ________________ОСНОВНЫЕ ПЕРЕМЕННЫЕ_______________________________________
-var setup = document.querySelector('.setup');
-setup.classList.remove('hidden');
-
-var similarListElement = document.querySelector('.setup-similar-list');
-var silimarWizardTemplate = document.querySelector('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
+// сделать wizards пустой массив и добавлять туда обьекты с данными
+// обернуть весь код  в функции, и вызывать их после написанного кода функций
+// если использую переменную глобально писать windows.название переменной
+// сделать отдельные функции для хидден
+// 
 // ___________ ДАННЫЕ ____________________________________________
 // ___Массив ИМЕН_____________
 var Names = [
@@ -48,54 +45,55 @@ var EyesColors = [
   'rgb(255, 255, 0)',
   'rgb(0, 128, 0)'
 ];
-// _____________________ОБЪЕКТ С 4 ВОЛШЕБНИКАМИ________________________
-var wizards = [
-  {
-    name: '',
-    coatColor: '',
-    eyesColor: ''
-  },
-  {
-    name: '',
-    coatColor: '',
-    eyesColor: ''
-  },
-  {
-    name: '',
-    coatColor: '',
-    eyesColor: ''
-  },
-  {
-    name: '',
-    coatColor: '',
-    eyesColor: ''
-  }
-];
-// ___________________ФУНКЦИИ И ДЕЙСТВИЯ_________________________
-for (var i = 0; i < wizards.length; i++) {
-  wizards[i].name = [randomParam(Names) + ' ' + randomParam(Surnames)];
-  wizards[i].coatColor = [randomParam(CoatColors)];
-  wizards[i].eyesColor = [randomParam(EyesColors)];
+// _____________________МАССИВ ДЛЯ ЗАПОЛНЕНИЯ 4-Х  КАЛДУНОВ________________________
+var wizards = [];
+// ____________________ФУНКЦИИ_______________________
+function show(searchClass, removeNameClass) {
+  searchClass.classList.remove(removeNameClass || 'hidden');
 };
-// цикл по подстановки в каждый обьект и каждый параметр нужного значения из массивов
+// _______________________________________________________________
+function createObjectWizard() {
+  var numberWizards = 4;
+  
+  for (var i = 0; i < numberWizards; i++) {
+    var obj = {
+      name: randomParam(Names) + ' ' + randomParam(Surnames),
+      coatColor: randomParam(CoatColors),
+      eyesColor: randomParam(EyesColors)
+    };
+    wizards.push(obj)[i];
+  };
+};
+// _______________________________________________________________
 function renderWizard(wizard) {
+  var silimarWizardTemplate = document.querySelector('#similar-wizard-template')
+      .content
+      .querySelector('.setup-similar-item');
   var wizardElement = silimarWizardTemplate.cloneNode(true);
+
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
   return wizardElement;
 };
-// кладем в функцию номер волшебника из массива, и добавляем для каждого 3 характеристики
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
+// _______________________________________________________________
+function insertFragment() {
+  var silimarListElement = document.querySelector('.setup-similar-list');
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  };
+  silimarListElement.appendChild(fragment);
 };
-
-similarListElement.appendChild(fragment);
-setup.querySelector('.setup-similar').classList.remove('hidden');
-
+// _______________________________________________________________
 function randomParam(arrParam) {
   var randomIndexArray = Math.floor(Math.random() * arrParam.length);
+
   return arrParam[randomIndexArray];
 };
-// Получаем случайный ключ массива
+// _______________________________________________________________
+show(document.querySelector('.setup-similar'));
+show(document.querySelector('.setup'));
+createObjectWizard();
+insertFragment();
