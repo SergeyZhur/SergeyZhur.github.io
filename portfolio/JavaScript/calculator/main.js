8d6323dab5511473870bd911911568fc3605327f
Sergei <ser90593696@yandex.ru>
2020-07-05 19:39:12 +0300

    корректировки

--- portfolio/JavaScript/calculator/main.js
+++ portfolio/JavaScript/calculator/main.js
@@ -1,54 +1,113 @@
 const valueButton = document.querySelector('#wrapper-btn');
+let result = document.querySelector('.text-result');
+let s = document.querySelector('#s');
 let arr = [];
+const actions = ['+', '-', '/', '*'];
+
+let strNumbers = '';
 const getValueAtr = (event) => {
+  strNumbers = ''
+  const lastElement = arr[arr.length - 1];
+  const { textContent } = event.target;
+
+  if (actions.includes(textContent)) {
+    if (actions.includes(lastElement)) {
+      arr[arr.length - 1] = textContent;
+    } else {
+      if (arr.length || textContent === '-') {
+        arr.push(event.target.textContent);
+      }
+    }
+  } else {
+    arr.push(event.target.textContent);
+  }
   
 
 
-arr.push(event.target.dataset.id);
-  console.log(arr)
-//     let sum = 0;
-//   for(let i = 0; i<arr.length; i++) {
-    
-//      sum = sum + arr[i];
-//     if(arr[i] === '+') {
-//       arr[i] = '+'
-//     }
-//   }
-//    let a = sum.substr(1);
-//   console.log(a);
+for(let i = 0; i < arr.length; i++) {
+
+  strNumbers = strNumbers + arr[i];
+  // if (arr[i] === '=') {
+  //   arr[i] = '';
+  //   let v = eval(strNumbers)
+  //   console.log(v);
+  // }
 }
+  
+
+
+
+  // console.log(strNumbers)
+ 
+ 
+  result.textContent = strNumbers;
+
+
+  s.addEventListener('click', function () {
+    let p = eval(strNumbers);
+    console.log(p)
+  })
+}
+
+
+  
+  
+
+ 
+
+
 
 valueButton.addEventListener('click', getValueAtr);
 
+// s.addEventListener('click', function() {
+//   
+//   return r
+// })
 
-  // при клике на нужный блок с цифрой, получаю его data атрибут, а именно значение
+const arr1 = ['1','-','2','*','5', '+', '4', '5'];
+let result = 0;
 
-// ['331+1111+0-5']
-// if(event === '+') {
-
-// }
-
-// function w( one, two) {
-// return one + two
-// }
-// let res = w(3,3)
-// console.log(res)
-
-
-
-// есть главный массив результатов
-
-
-// массив сложения
-// массив вычитания
-// массив умножения 
-
-let as = ['3','2','-','5'];
-let r = [];
-for(let i = 0; i < as.length; i++) {
-
-  if(typeof string) {
-    r.push(Number(as[i]))
-    console.log(r)
-  }
+export const actions = {
+  ymnojenie: '*',
+  delenie: '/',
+  plus: '+',
+  minus: '-'
 }
+
+export const actionsFunctions = {
+  [actions.ymnojenie]: ymnojenie,
+  [actions.delenie]: delenie,
+  [actions.plus]: plus,
+  [actions.minus]: minus
+}
+
+actionsFunctions[actions.minus]
+
+arr1.forEach((n, i) => {
+  if (i === 0) {
+    const callback = actions[arr1[i + 1]];
+    result = callback(+n, arr1[i + 2]);
+    return;
+  }
+  if (isFinite(+n) && i > 2) {
+    const callback = actions[arr1[i - 1]] ?? plus;
+    result = callback(result, +n);
+  }
+});
+
+function plus(num, num2) {
+  return num + num2
+}
+
+function minus(num, num2) {
+  return num - num2
+}
+
+function delenie(num, num2) {
+  return num / num2
+}
+
+function ymnojenie(num, num2) {
+  return num * num2
+}
+
