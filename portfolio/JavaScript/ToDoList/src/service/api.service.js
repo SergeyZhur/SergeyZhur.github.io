96e92b111dd68b78833925a53bd8b5741c4319a3
Sergei <ser90593696@yandex.ru>
2020-07-11 22:10:53 +0300

    работа с компонентами и кнопками

--- portfolio/JavaScript/ToDoList/src/service/api.service.js
+++ portfolio/JavaScript/ToDoList/src/service/api.service.js
@@ -16,6 +16,23 @@ class ApiService {
     }
 
   }
+
+  async fetchPosts() {
+    try {
+   const request = new Request(`${this .url}/posts.json`, {
+     method: 'get'
+   })
+   return useRequest(request)
+    } catch(error) {
+       console.error(error)
+    }
+  }
+}
+
+
+async function useRequest(request) {
+  const response = await fetch(request)
+  return await response.json()
 }
 
 export const apiService = new ApiService('https://prodject-js-todolist.firebaseio.com')
